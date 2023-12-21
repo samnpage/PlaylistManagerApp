31b2538a2f7ba75d6e919c46a1e8cfc309317e6a
Sam Page <samnpage96@gmail.com>
2023-12-21 18:45:49 -0500

    User identity dependency injection added

--- PlaylistManagerApp.Data/PlaylistManagerDbContext.cs
+++ PlaylistManagerApp.Data/PlaylistManagerDbContext.cs
@@ -1,9 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PlaylistManagerApp.Data.Entities;
 
 namespace PlaylistManagerApp.Data;
 
-public class PlaylistManagerDbContext : DbContext
+public class PlaylistManagerDbContext : IdentityDbContext<UserEntity, IdentityRole<int>, int>
 {
     public PlaylistManagerDbContext(DbContextOptions<PlaylistManagerDbContext> options)
         : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserEntity>().ToTable("Users");
+    }
 }
