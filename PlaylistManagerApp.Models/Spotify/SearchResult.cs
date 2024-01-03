2fcfd521d1dcbfb5f69b43fdb00bcc70e20b247b
Sam Page <samnpage96@gmail.com>
2024-01-02 19:44:20 -0500

    changed to songs

--- PlaylistManagerApp.Models/Spotify/SearchResult.cs
+++ PlaylistManagerApp.Models/Spotify/SearchResult.cs
@@ -1,10 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace PlaylistManagerApp.Models.Spotify;
+
 public class SearchResult
 {
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public List<SearchArtist> Artists { get; set; } = null!;
+    public List<SearchSong> Songs { get; set; } = null!;
     public string Uri { get; set; } = string.Empty;
 }
