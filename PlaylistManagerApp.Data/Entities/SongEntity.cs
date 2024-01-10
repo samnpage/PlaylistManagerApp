404c226886e97cde0d18e76b4bcb294768d16894
Sam Page <samnpage96@gmail.com>
2024-01-09 21:04:22 -0500

    removed using statement

--- PlaylistManagerApp.Data/Entities/SongEntity.cs
+++ PlaylistManagerApp.Data/Entities/SongEntity.cs
@@ -1,11 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlaylistManagerApp.Data.Entities;
 
 public class SongEntity
 {
     [Key]
-    public int SongId { get; set; }
+    public int SongId { get; set; }    
     public string SpotifyTrackID { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Artist { get; set; } = string.Empty;
