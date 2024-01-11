4297ebd4f0e1fa5ff33ce7547cc8fe36d4b612b9
Sam Page <samnpage96@gmail.com>
2024-01-10 20:06:41 -0500

    initial commit

--- PlaylistManagerApp.Models/PlaylistSong/PlaylistSongListItem.cs
+++ PlaylistManagerApp.Models/PlaylistSong/PlaylistSongListItem.cs
@@ -2,5 +2,9 @@ namespace PlaylistManagerApp.Models.PlaylistSong;
 
 public class PlaylistSongListItem
 {
-    
+    public int PlaylistSongId { get; set; }
+    public int SongId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+    public DateTime DateAdded { get; set; }
 }
