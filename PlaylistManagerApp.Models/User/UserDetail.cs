b33816de242e66c46c134213c2fd8631c9bfc6e4
Sam Page <samnpage96@gmail.com>
2024-01-20 20:50:33 -0500

    added email and username properties

--- PlaylistManagerApp.Models/User/UserDetail.cs
+++ PlaylistManagerApp.Models/User/UserDetail.cs
@@ -2,7 +2,9 @@ namespace PlaylistManagerApp.Models.User;
 
 public class UserDetail
 {
-    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Email { get; set; } = null!;
+    public string Username { get; set; } = null!;
     public string Name { get; set; } = string.Empty;
     public DateTimeOffset RegistrationDate { get; set; }
 }
